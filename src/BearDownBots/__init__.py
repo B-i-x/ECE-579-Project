ebdef061cf99a9b84a1f4bb0536377a455f79c35
B-i-x <bix.romero@gmail.com>
2025-04-28 20:16:11 -0700

    added fast mode

--- src/BearDownBots/__init__.py
+++ src/BearDownBots/__init__.py
@@ -1,9 +1,18 @@
 # # src/BearDownBots/__init__.py
 
 from BearDownBots.app import BearDownBotsApp
-
+from BearDownBots.config import Config
 def main():
     app = BearDownBotsApp()
 
     app.run()
+
+def fast():
+    """
+    Fast mode for running the application without GUI.
+    """
+    Config.Environment.MAP_ROWS = 1000
+    Config.Environment.MAP_COLS = 1000
+
+    main()
 
--- src/BearDownBots/config.py
+++ src/BearDownBots/config.py
@@ -13,5 +13,5 @@ class Config:
         MAP_ROWS  = 2000
         MAP_COLS  = 2000
         MAX_BUILDING_ATTEMPTS = 100 ## the higher the number, the denser the campus is
-        MIN_BUILDING_CELLS = 100
-        MAX_BUILDING_CELLS = 250
+        MIN_BUILDING_CELLS = 75
+        MAX_BUILDING_CELLS = 200

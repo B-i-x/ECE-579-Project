c60686d7710aa6ebe2e56287c11f595abea1c4e7
B-i-x <bix.romero@gmail.com>
2025-05-12 23:58:51 -0700

    ez fix, 3 lines of code

--- src/BearDownBots/static/cell.py
+++ src/BearDownBots/static/cell.py
@@ -19,12 +19,12 @@ class CustomCellTypeEnum(Enum):
     
 class CELL_TYPES(CustomCellTypeEnum):
     RESTUARANT_PICKUP = (6, "#e06192")  # pink
-    ROBOT      = (4, "#00008B")   # dark blue
     RESTAURANT = (5, "#e6eb14")   # yellow
     BUILDING_DROP_OFF = (7, "#ff8c00")  # dark orange
     GROUND     = (0, "#c1e8c2")   # dark green
     BUILDING   = (1, "#A0522D")   # sienna
     OBSTACLE   = (3, "#8B0000")   # dark red
+    ROBOT      = (4, "#555555")   # dark gray
     WALKWAY    = (2, "#555555")   # dark gray
 
 
